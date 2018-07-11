f052f12dfd8876dbc458fff90976e65d063474f6
stan <nonamegod>
2018-07-11 17:59:10 +0800

    my second commit

---
+++ app/src/main/java/com/cas/stan/downloaddemo/TestEvent.java
@@ -0,0 +1,12 @@
+package com.cas.stan.downloaddemo;
+
+class TestEvent {
+    private double mMsg;
+
+    public double getMsg(){
+        return mMsg;
+    }
+    public TestEvent(double msg) {
+        mMsg = msg;
+    }
+}
