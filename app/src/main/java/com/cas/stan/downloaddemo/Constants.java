0ac84c18bf23ab54d6a0dadf11461cc84373502a
stan <nonamegod>
2018-07-11 17:53:39 +0800

    my first commit

--- app/src/main/java/com/cas/stan/downloaddemo/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.cas.stan.downloaddemo;
-
-public class Constants {
-    public static final String SAVE_PATH = "mnt/sdcard/Download/";
-    public static final String HTTP_BASEURL = "http://surl.qq.com/";
-    public static final String filaName = "downloadDemo.apk";
-}
